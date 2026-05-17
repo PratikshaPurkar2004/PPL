5b9370e2eeb3d9ca3d2f19d695013edae55b2393
PratikshaPurkar2004 <purkarpratiksha00@gmail.com>
2026-05-17 22:48:09 +0530

    17-05-2026

---
+++ PPL2/ConsicutiveOnes.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    int n,arr[n];
+    cout<<"enter array size: ";
+    cin>>n;
+
+    cout<<"enter array::";
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+
+    int cnt=0,ans=0;
+    for(int i=0;i<n;i++)
+        if(arr[i]==1)
+        {
+            cnt++;
+            ans=max(cnt,ans);
+        }
+        else
+        {
+            cnt=0;
+        }
+    cout<<"max consecutive ones are:: "<<ans;
+    return 0;
+}
