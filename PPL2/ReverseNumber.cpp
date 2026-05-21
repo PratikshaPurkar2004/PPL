5bec3f3253788b1e4f85b1d3182defeeddf47914
PratikshaPurkar2004 <purkarpratiksha00@gmail.com>
2026-05-21 18:46:16 +0530

    21-05-2026

---
+++ PPL2/ReverseNumber.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+#include<climits>
+class Reverse
+{
+    public:
+        int reverse(int x)
+        {
+            int ans=0;
+            int MAX=INT_MAX/10,MIN=INT_MIN/10;
+            while(x!=0)
+            {
+                if(MAX<ans || MIN>ans)
+                    return 0;
+                ans=ans*10+(x%10);
+                x/=10;
+            }
+            return ans;
+        }
+};
+
+int main()
+{
+    int x;
+    cout<<"Enter Number::";
+    cin>>x;
+    Reverse r;
+    cout<<r.reverse(x);
+    return 0;
+}
