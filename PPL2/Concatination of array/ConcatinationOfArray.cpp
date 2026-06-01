86b95658f7fe2e333c7600cf4883d7e87e92b7f7
PratikshaPurkar2004 <purkarpratiksha00@gmail.com>
2026-06-01 22:16:50 +0530

    changes in function

--- PPL2/Concatination of array/ConcatinationOfArray.cpp
+++ PPL2/Concatination of array/ConcatinationOfArray.cpp
@@ -5,25 +5,44 @@ using namespace std;
 
 void ConcatinationOfArray::input()
 {
-    int n;
-    cout<<"Enter size::";
-    cin>>n;
-    cout<<"Enter array::";
-    for(int i=0;i<n;i++)    
-        cin>>nums[i];
+    cout << "Enter size:";
+    cin >> n;
 
+    nums.resize(n);
+
+    cout << "Enter array:";
+    for(int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
 }
 
-vector <int> ConcatinationOfArray:: getConcatenation()
+// vector <int> ConcatinationOfArray:: getConcatenation()
+// {
+//     int n=nums.size();
+//     vector<int>ans(2*n);
+//     for(int i=0;i<n;i++)
+//     {
+//         ans[i]=nums[i];
+//         ans[i+n]=nums[i];
+//     }
+//     return ans;
+// }
+
+
+vector<int> ConcatinationOfArray::getConcatenation()
 {
-    int n=nums.size();
-    vector<int>ans(2*n);
-    for(int i=0;i<n;i++)
+    vector<int> ans;
+
+    for(int i = 0; i < n; i++)
     {
-        ans[i]=nums[i];
-        ans[i+n]=nums[i];
+        ans.push_back(nums[i]);
     }
+
+    for(int i = 0; i < n; i++)
+    {
+        ans.push_back(nums[i]);
+    }
+
     return ans;
 }
-
-
