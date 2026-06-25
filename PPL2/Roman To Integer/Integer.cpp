ed6dd5cad16b1d8b438d867635a56147ea5bd895
PratikshaPurkar2004 <purkarpratiksha00@gmail.com>
2026-06-25 19:21:16 +0530

    25-06-2026

--- PPL2/Roman To Integer/Integer.cpp
+++ PPL2/Roman To Integer/Integer.cpp
@@ -10,49 +10,59 @@ void Integer::input()
    cin>>s; 
 }
 
-// int Integer:: convert()
-// {
-//     unordered_map<string,int> map = {
-//     {"I",1}, {"V",5}, {"X",10},
-//     {"L",50}, {"C",100},
-//     {"D",500}, {"M",1000},
-//     {"IV",4}, {"IX",9},
-//     {"XL",40}, {"XC",90},
-//     {"CD",400}, {"CM",900}
-// };
-//     int ans=0;
-//     for(int i=0;i<s.size();i++)
-//     {
-//         string s1;
-//         if(i != s.size()-1)
-//         {
-//             s1 += s[i];
-//             s1 += s[i+1];
-//         }
-//         else
-//             s1 += s[i];
-//     }
-//     return ans;
-// }
 
+int Integer::convert(){
+unordered_map<string, int> mp = {
+    {"I",1}, {"V",5}, {"X",10}, {"L",50},
+    {"C",100}, {"D",500}, {"M",1000},
+    {"IV",4}, {"IX",9}, {"XL",40},
+    {"XC",90}, {"CD",400}, {"CM",900}
+};
 
-int Integer::convert()
+int ans = 0;
+
+for(int i = 0; i < s.size(); i++)
 {
-    unordered_map<char,int> mp = {
-        {'I',1}, {'V',5}, {'X',10},
-        {'L',50}, {'C',100},
-        {'D',500}, {'M',1000}
-    };
+    string str = "";
+    str += s[i];
 
-    int ans = 0;
-
-    for(int i = 0; i < s.size(); i++)
+    if(i + 1 < s.size())
     {
-        if(i + 1 < s.size() && mp[s[i]] < mp[s[i+1]])
-            ans -= mp[s[i]];
-        else
-            ans += mp[s[i]];
+        str += s[i + 1];
+
+        if(mp.count(str))
+        {
+            ans += mp[str];
+            i++;          // Skip next character
+            continue;
+        }
+
+        str.pop_back();   // Remove second character
     }
 
-    return ans;
+    ans += mp[str];
 }
+
+return ans;
+}
+
+// int Integer::convert()
+// {
+//     unordered_map<char,int> mp = {
+//         {'I',1}, {'V',5}, {'X',10},
+//         {'L',50}, {'C',100},
+//         {'D',500}, {'M',1000}
+//     };
+
+//     int ans = 0;
+
+//     for(int i = 0; i < s.size(); i++)
+//     {
+//         if(i + 1 < s.size() && mp[s[i]] < mp[s[i+1]])
+//             ans -= mp[s[i]];
+//         else
+//             ans += mp[s[i]];
+//     }
+
+//     return ans;
+// }
