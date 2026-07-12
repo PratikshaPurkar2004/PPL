0d68b5ce7fb641111648e5566f21a2ee9fb91695
PratikshaPurkar2004 <purkarpratiksha00@gmail.com>
2026-07-12 20:42:09 +0530

    remove duplicate of unsorted array

---
+++ PPL2/Remove Duplicate/Array.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<unordered_set>
+#include<vector>
+using namespace std;
+
+class Solution
+{
+    vector<int>arr;
+    public:
+        void input(int &n)
+        {
+            cout<<"Enter a array of size::";
+            cin>>n;
+            arr.resize(n);
+            cout<<"Enter array elements::";
+            for(int i=0;i<n;i++)
+                cin>>arr[i];
+        }
+
+        void removeDuplicate()
+        {
+            unordered_set<int>s;
+            for(int i=0;i<arr.size();i++)
+            {
+                if(s.find(arr[i])==s.end())
+                {
+                    cout<<arr[i]<<" ";
+                    s.insert(arr[i]);
+                }
+            }
+        }
+};
+
+int main()
+{
+    Solution s;
+    int n;
+    s.input(n);
+    s.removeDuplicate();
+    return 0;
+}
