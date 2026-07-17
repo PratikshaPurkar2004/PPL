f373ec3a86ee04af681915fed15de8ae01818686
PratikshaPurkar2004 <purkarpratiksha00@gmail.com>
2026-07-17 22:02:58 +0530

    17-07-2026

---
+++ PPL2/Valid Parenthese/Solution.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Solution
+{
+    string s;
+    public:
+        void input()
+        {
+            cout<<"Enter a string::";
+            getline(cin,s);
+        }
+        bool validParentheses()
+        {
+            stack<char>stk;
+            for(char ch:s)
+            {
+                if(ch=='(' || ch=='{' || ch=='[')
+                    stk.push(ch);
+                else
+                {
+                    if(stk.empty())
+                        return false;
+                    char top=stk.top();
+                    stk.pop();
+                    if((ch==')' && top!='(' )||(ch=='}' && top!='{') || (ch==']' && top!='['))
+                        return false;
+                }
+            }
+           // return stk.empty();
+        }
+};
+
+int main()
+{
+    Solution s;
+    s.input();
+    if(s.validParentheses())
+        cout<<"Valid Parentheses";
+    else
+        cout<<"Invalid-Parenthese";
+    return 0;
+}
