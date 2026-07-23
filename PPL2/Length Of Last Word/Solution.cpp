29852fc10fa7f3d346e1ade5b8d23650447afa8b
PratikshaPurkar2004 <purkarpratiksha00@gmail.com>
2026-07-23 20:24:16 +0530

    22-07-2026

--- PPL2/Length Of Last Word/Solution.cpp
+++ PPL2/Length Of Last Word/Solution.cpp
@@ -11,26 +11,26 @@ class Solution
         cin>>s;
     }
     int lengthOfLastWord()
-    {
-        int end=s.size()-1;
-        while(s[end]==' ')
-            end--;
-        int start=end;
-        while(start>=0 && s[start]!=' ')
-            start--;
-        return end-start;
-    }
     // {
-    //     int cnt=0;
-    //     for(int i=s.size()-1;i>=0;i--)
-    //     {
-    //         if(s[i]==' ' && cnt>0)
-    //             break;
-    //         else if(s[i]!=' ')
-    //             cnt++;
-    //     }
-    //     return cnt;
-    //}
+    //     int end=s.size()-1;
+    //     while(s[end]==' ')
+    //         end--;
+    //     int start=end;
+    //     while(start>=0 && s[start]!=' ')
+    //         start--;
+    //     return end-start;
+    // }
+    {
+        int cnt=0;
+        for(int i=s.size()-1;i>=0;i--)
+        {
+            if(s[i]==' ' && cnt>0)
+                break;
+            else if(s[i]!=' ')
+                cnt++;
+        }
+        return cnt;
+    }
 };
 
 int main()
