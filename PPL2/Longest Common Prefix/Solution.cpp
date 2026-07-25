06e6ee109f17d991fcdd6306cc92e10a9a8d3a23
PratikshaPurkar2004 <purkarpratiksha00@gmail.com>
2026-07-25 20:17:01 +0530

    25-07-2026

---
+++ PPL2/Longest Common Prefix/Solution.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+    vector<string> str;
+    public:
+        void input()
+        {
+            cout<<"Enter the string: ";
+            string s;
+            getline(cin,s);
+            str.push_back(s);
+        }
+        string longestCommonPrefix()
+        {
+            string ans="";
+            int n=str.size();
+            for(int i=0;i<str[0].size();i++)
+            {
+                char ch=str[0][i];
+                for(int j=1;j<n;j++)
+                {
+                    if(str[j][i]!=ch)
+                        return ans;
+                }
+                ans+=ch;
+            }
+            return ans;
+        }
+};
+
+int main()
+{
+    Solution s;
+    int n;
+    cout<<"Enter the number of strings: ";
+    cin>>n;
+    cin.ignore();
+    for(int i=0;i<n;i++)
+        s.input();
+    cout<<"Longest Common Prefix: "<<s.longestCommonPrefix();
+    return 0;
+}
