e29b23bd1378ad6538555827df6673f40dc54cca
PratikshaPurkar2004 <purkarpratiksha00@gmail.com>
2026-07-30 21:07:22 +0530

    30-07-2026

---
+++ PPL2/Trapping Rain Water/Solution.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution
+{
+    public:
+        vector<int>height;
+        int n;
+        void input()
+        {
+            cout<<"Enter a size of array::";
+            cin>>n;
+            height.resize(n);
+            cout<<"enter array elements::";
+            for(int i=0;i<n;i++)
+                cin>>height[i];
+        }
+
+        int trap()
+        {
+            int leftMax=0,rightMax=0,left=0,right=n-1;
+            int water=0;
+            while(left<right)
+            {
+                if(height[left]<height[right])
+                {
+                    leftMax=max(leftMax,height[left]);
+                    water+=leftMax-height[left];
+                    left++;
+                }
+                else
+                {
+                    rightMax=max(rightMax,height[right]);
+                    water+=rightMax-height[right];
+                    right--;
+                }
+            }
+            cout<<water;
+            return 0;
+        }
+};
+
+
+int main()
+{
+    Solution s;
+    s.input();
+    s.trap();
+    return 0;
+}
