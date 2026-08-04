16e26b4cb3c6c0ef9c91464abc19e5fab68f7231
PratikshaPurkar2004 <purkarpratiksha00@gmail.com>
2026-08-04 22:34:20 +0530

    04-08-2026

--- PPL2/Find Vowel and Consonent/Solution.cpp
+++ PPL2/Find Vowel and Consonent/Solution.cpp
@@ -4,19 +4,39 @@ using namespace std;
 class Solution
 {
     public:
-        char ch;
+        // char ch;
+        // void input()
+        // {
+        //     cout<<"Enter a character::";
+        //     cin>>ch;
+        // }
+
+        // void findVowelConsonent()
+        // {
+        //         if(ch=='a' || ch=='e' || ch=='i' || ch== 'o' || ch=='u')
+        //             cout<<"Vowels";
+        //         else
+        //             cout<<"Consonent";
+        // }
+
+        string str;
         void input()
         {
-            cout<<"Enter a character::";
-            cin>>ch;
+            cout<<"Enter a string::";
+            cin>>str;
         }
-
         void findVowelConsonent()
         {
-                if(ch=='a' || ch=='e' || ch=='i' || ch== 'o' || ch=='u')
-                    cout<<"Vowels";
+            int v=0,c=0;
+            for(int i=0;i<str.length();i++)
+            {
+                if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]== 'o' || str[i]=='u')
+                    v++;
                 else
-                    cout<<"Consonent";
+                    c++;
+            }
+            cout<<"Vowels::"<<v<<endl;
+            cout<<"Consonent::"<<c<<endl;
         }
 };
 
